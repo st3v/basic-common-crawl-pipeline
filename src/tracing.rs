5486d1fe8a4a21c544257647dccc6189f4785c1d
Stev Witzel <stev.witzel@gmail.com>
2025-01-20 13:57:30 +0100

    Use  as default if RUST_LOG has not been set

--- src/tracing.rs
+++ src/tracing.rs
@@ -2,10 +2,10 @@
 use tracing_subscriber::EnvFilter;
 
 /// Constructs a tracing subscriber that prints formatted traces to stdout.
-/// Default level is `error` but can be configured via the `RUST_LOG` environment variable.
+/// Default level is `info` but can be configured via the `RUST_LOG` environment variable.
 /// Registers that subscriber to process traces emitted after this point.
 pub fn setup() {
-    let filter = EnvFilter::from_default_env();
+    let filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info"));
     let subscriber = tracing_subscriber::fmt().with_env_filter(filter).finish();
     tracing::subscriber::set_global_default(subscriber).unwrap();
     tracing::info!("Tracing initialized");
